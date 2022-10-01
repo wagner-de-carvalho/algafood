1bbd9400bccf447ee9fb5036631601ce579782fd
wagner <wagncarv@outlook.com>
2022-10-01 16:50:40 -0300

    3.19. Propriedade nullable de @Column e @JoinColumn

--- src/main/java/com/home/algafood/domain/model/Restaurante.java
+++ src/main/java/com/home/algafood/domain/model/Restaurante.java
@@ -2,6 +2,7 @@ package com.home.algafood.domain.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,12 +22,14 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
+    @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
-    
+
     @ManyToOne
-    @JoinColumn(name="cozinha_id")
+    @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
 }
