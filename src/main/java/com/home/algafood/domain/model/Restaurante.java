3c0a58cf0da76574e9f2a7a4716fd488849857b8
wagner <wagncarv@outlook.com>
2022-10-01 16:09:39 -0300

    3.17. Mapeando relacionamento com @ManyToOne

--- src/main/java/com/home/algafood/domain/model/Restaurante.java
+++ src/main/java/com/home/algafood/domain/model/Restaurante.java
@@ -1,59 +1,30 @@
 package com.home.algafood.domain.model;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
-	@Id
-	private Long id;
-	private String nome;
-	@Column(name = "taxa_frete")
-	private BigDecimal taxaFrete;
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    private String nome;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public BigDecimal getTaxaFrete() {
-		return taxaFrete;
-	}
-
-	public void setTaxaFrete(BigDecimal taxaFrete) {
-		this.taxaFrete = taxaFrete;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Restaurante other = (Restaurante) obj;
-		return Objects.equals(id, other.id);
-	}
+    private BigDecimal taxaFrete;
+    
+    @ManyToOne
+    private Cozinha cozinha;
 
 }
---
+++ src/main/java/com/home/algafood/domain/repository/RestauranteRepository.java
@@ -0,0 +1,16 @@
+package com.home.algafood.domain.repository;
+
+import java.util.List;
+
+import com.home.algafood.domain.model.Restaurante;
+
+public interface RestauranteRepository {
+    List<Restaurante> listar();
+
+    Restaurante buscar(Long id);
+
+    Restaurante salvar(Restaurante restaurante);
+
+    void remover(Restaurante restaurante);
+
+}
