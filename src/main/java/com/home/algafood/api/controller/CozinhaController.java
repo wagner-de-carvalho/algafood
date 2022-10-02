662b2a206aee8ce8939e606e029c6a81f44658cf
wagner <wagncarv@outlook.com>
2022-10-02 17:02:41 -0300

    4.16. Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/com/home/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/home/algafood/api/controller/CozinhaController.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.home.algafood.api.model.CozinhasXmlWrapper;
 import com.home.algafood.domain.model.Cozinha;
 import com.home.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping(value = "/cozinhas", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 
     @Autowired
@@ -22,6 +23,11 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml() {
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
     }
 
     @GetMapping("/{cozinhaId}")
--- src/main/java/com/home/algafood/domain/model/Cozinha.java
+++ src/main/java/com/home/algafood/domain/model/Cozinha.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -22,8 +21,6 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @JsonIgnore
-    @JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
 
