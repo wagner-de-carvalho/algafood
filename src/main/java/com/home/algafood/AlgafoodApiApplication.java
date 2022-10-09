c5cace550e1b7916bb862db0fd1c7f370a6e8cc2
wagner <wagncarv@outlook.com>
2022-10-09 20:21:01 -0300

    5.20. Estendendo o JpaRepository para customizar o repositório base

--- src/main/java/com/home/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/home/algafood/AlgafoodApiApplication.java
@@ -2,12 +2,16 @@ package com.home.algafood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.home.algafood.infrastructure.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlgafoodApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlgafoodApiApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/home/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.home.algafood.infrastructure.repository;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import com.home.algafood.domain.repository.CustomJpaRepository;
+
+public class CustomJpaRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID> implements CustomJpaRepository<T, ID> {
+
+    @Autowired
+    private EntityManager manager;
+
+    public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+        this.manager = entityManager;
+    }
+
+    @Override
+    public Optional<T> buscarPrimeiro() {
+        var jpql = "from " + getDomainClass().getName();
+        T entity = manager.createQuery(jpql, getDomainClass())
+                .setMaxResults(1)
+                .getSingleResult();
+        return Optional.ofNullable(entity);
+    }
+
+}
