4c2dad0c04a403e27e4f3057b5aeeb24e65eb2d8
wagner <wagncarv@outlook.com>
2022-10-15 23:49:39 -0300

    6.6. Mapeando propriedades com @CreationTimestamp e @UpdateTimestamp

--- src/main/java/com/home/algafood/domain/model/Restaurante.java
+++ src/main/java/com/home/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.home.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -39,8 +43,19 @@ public class Restaurante {
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
     
+    @JsonIgnore
     @Embedded
     private Endereco endereco;
+    
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+    
+    @JsonIgnore
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataAtualizacao;
 
     @JsonIgnore
     @ManyToMany
