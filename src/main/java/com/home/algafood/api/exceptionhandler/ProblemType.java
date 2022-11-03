6983daafa97a85c74ef9ae1235444220dda7443c
wagner <wagncarv@outlook.com>
2022-11-02 23:02:05 -0300

    8.27. Desafio: tratando outras exceções não capturadas

--- src/main/java/com/home/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/home/algafood/api/exceptionhandler/ProblemType.java
@@ -7,6 +7,7 @@ public enum ProblemType {
 
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
+	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
 	MENSAGEM_INCOMPREENSIVEL("mensagem-incompreensivel", "Mensagem incompreensível"),
 	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"); 
