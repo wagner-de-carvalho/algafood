afc972d3bb0f83e61b8cd3a61f4e9bad589c6f36
wagner <wagncarv@outlook.com>
2022-11-02 23:27:17 -0300

    8.29. Desafio: estendendo o formato do problema

--- src/main/java/com/home/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/home/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.home.algafood.api.exceptionhandler;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,7 +10,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -85,19 +85,34 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@Override
-	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		if (body == null) {
-			body = Problem.builder().title(status.getReasonPhrase()).status(status.value()).build();
-		} else if (body instanceof String) {
-			body = Problem.builder().title((String) body).status(status.value()).build();
-		}
-
-		return super.handleExceptionInternal(ex, body, headers, status, request);
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+	        HttpStatus status, WebRequest request) {
+	    
+	    if (body == null) {
+	        body = Problem.builder()
+	            .timestamp(LocalDateTime.now())
+	            .title(status.getReasonPhrase())
+	            .status(status.value())
+	            .userMessage(MSG_ERRO_GENERICO_USUARIO_FINAL)
+	            .build();
+	    } else if (body instanceof String) {
+	        body = Problem.builder()
+	            .timestamp(LocalDateTime.now())
+	            .title((String) body)
+	            .status(status.value())
+	            .userMessage(MSG_ERRO_GENERICO_USUARIO_FINAL)
+	            .build();
+	    }
+	    
+	    return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 
 	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
-		return Problem.builder().status(status.value()).type(problemType.getUri()).title(problemType.getTitle())
+		return Problem.builder()
+				.timestamp(LocalDateTime.now())
+				.status(status.value())
+				.type(problemType.getUri())
+				.title(problemType.getTitle())
 				.detail(detail);
 	}
 
@@ -158,7 +173,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 						+ "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
 				ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
 
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(detail)
+				.build();
 
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
