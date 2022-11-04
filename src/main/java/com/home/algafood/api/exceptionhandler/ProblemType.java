c4b47f85a03bb344517c800d7daf826b67083d56
wagner <wagncarv@outlook.com>
2022-11-04 12:02:58 -0300

    9.3. Desafio: tratando exception de violação de constraints de validação

--- src/main/java/com/home/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/home/algafood/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
